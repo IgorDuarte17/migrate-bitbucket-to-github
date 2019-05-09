674e9902f6551f0dc11a3b5297b3949f0a504ebe
Igor Santos <igors.d@hotmail.com>
2019-05-09 16:06:23 -0300

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # set this as your github credentials
-GH_USER=user@domain.com 
-GH_PASSWORD=password  
-GH_USERNAME=UserName 
+GITHUB_USER=user@domain.com 
+GITHUB_PASSWORD=password  
+GITHUB_USERNAME=UserName 
 
 # set this as your bitbuckt credentials
-BB_USER=user@domain.com 
-BB_PASSWORD=password
-BB_USERNAME=UserName
+BITBUCKET_USER=user@domain.com 
+BITBUCKET_PASSWORD=password
+BITBUCKET_USERNAME=UserName
 
 set -e
-repos=$(bb list -u $BB_USER -p $BB_PASSWORD --private | grep $BB_USERNAME | cut -d' ' -f3 | cut -d'/' -f2)
+repos=$(bb list -u $BITBUCKET_USER -p $BITBUCKET_PASSWORD --private | grep $BITBUCKET_USERNAME | cut -d' ' -f3 | cut -d'/' -f2)
 for repo in $repos; do
   echo
   echo "* Processing $repo..."
   echo
-  git clone --bare git@bitbucket.org:$BB_USERNAME/$repo.git 
+  git clone --bare git@bitbucket.org:$BITBUCKET_USERNAME/$repo.git 
   cd $repo.git
   echo
   echo "* $repo cloned, now creating on github..."  
   echo
-  curl -u $GH_USER:$GH_PASSWORD https://api.github.com/user/repos -d "{\"name\": \"$repo\", \"private\": true}"
+  curl -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/user/repos -d "{\"name\": \"$repo\", \"private\": true}"
   echo
   echo "* mirroring $repo to github..."  
   echo
-  git push --mirror git@github.com:$GH_USERNAME/$repo.git && \
-  bb delete -u $BB_USER -p $BB_PASSWORD --owner $BB_USERNAME $repo    
+  git push --mirror git@github.com:$GITHUB_USERNAME/$repo.git && \
+  bb delete -u $BITBUCKET_USER -p $BITBUCKET_PASSWORD --owner $BITBUCKET_USERNAME $repo    
   cd ..  
 done
